544d181c475215ceb6270e87cd8ad04df968685a
Georg Dangl <georg@dangl.me>
2017-04-05 21:09:40 +0200

    Add script for testing & code coverage

---
+++ TestsAndCoverage.ps1
@@ -0,0 +1,36 @@
+$testProjects = "XmlTools.Tests"
+$testFrameworks = "net46", "netcoreapp1.1"
+$testRuns = 1;
+
+# Get the most recent OpenCover NuGet package from the dotnet nuget packages
+$nugetOpenCoverPackage = Join-Path -Path $env:USERPROFILE -ChildPath "\.nuget\packages\OpenCover"
+$latestOpenCover = Join-Path -Path ((Get-ChildItem -Path $nugetOpenCoverPackage | Sort-Object Fullname -Descending)[0].FullName) -ChildPath "tools\OpenCover.Console.exe"
+# Get the most recent OpenCoverToCoberturaConverter from the dotnet nuget packages
+$nugetCoberturaConverterPackage = Join-Path -Path $env:USERPROFILE -ChildPath "\.nuget\packages\OpenCoverToCoberturaConverter"
+$latestCoberturaConverter = Join-Path -Path (Get-ChildItem -Path $nugetCoberturaConverterPackage | Sort-Object Fullname -Descending)[0].FullName -ChildPath "tools\OpenCoverToCoberturaConverter.exe"
+
+foreach ($testProject in $testProjects){
+    foreach ($testFramework in $testFrameworks){
+        # Arguments for running dotnet
+        $dotnetArguments = "test", "`"`"$PSScriptRoot\test\$testProject`"`"", "-xml results_$testRuns.testresults"
+
+        "Running tests with OpenCover"
+        & $latestOpenCover `
+            -register:user `
+            -target:dotnet.exe `
+            "-targetargs:$dotnetArguments" `
+            -returntargetcode `
+            -output:"$PSScriptRoot\OpenCover.coverageresults" `
+            -mergeoutput `
+            -excludebyattribute:System.CodeDom.Compiler.GeneratedCodeAttribute `
+            "-filter:+[XmlTools*]* -[*.Tests]* -[*.Tests.*]*"
+
+        $testRuns++
+    }
+}
+
+"Converting coverage reports to Cobertura format"
+& $latestCoberturaConverter `
+    -input:"$PSScriptRoot\OpenCover.coverageresults" `
+    -output:"$PSScriptRoot\Cobertura.coverageresults" `
+    "-sources:$PSScriptRoot"
